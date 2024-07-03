2d691ab89e4123698ecce6f851e5c7cdb54d5d65
Darshil Bhuva <darshilbhuva4322@gmail.com>
2024-07-03 23:27:07 +0530

    add transactions datatable

--- src/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import {
   Card,
   CardContent,
@@ -17,22 +17,35 @@ import {
 } from "@/components/ui/breadcrumb";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import PaymentRecive from "@/assets/svg/PaymentReceive.svg";
+import PaymentReceive from "@/assets/svg/PaymentReceive.svg";
 import PaymentTransfer from "@/assets/svg/PaymentTransfer.svg";
-const Dashboard: React.FC = () => {
+import { Payment, columns } from "@/components/payments/columns";
+import { DataTable } from "@/components/payments/DataTable";
+async function getData(): Promise<Payment[]> {
+  return new Array(85).fill(null).map(() => ({
+    id: "728ed52f",
+    amount: Math.random() * 10000,
+    status: "pending",
+    email: "m@example.com",
+  }));
+}
+
+const Dashboard = () => {
+  const [data, setData] = useState<Payment[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getData();
+      setData(result);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className="mb-8">
@@ -76,48 +89,50 @@ const Dashboard: React.FC = () => {
         </TabsList>
         <TabsContent value="Overview">
           <div className="grid grid-cols-4 max-md:grid-cols-1 gap-8">
-            <Card className="border-0">
-              <CardHeader>
-                <CardTitle>
-                  <div className="flex items-center justify-between mb-3">
-                    <div className="flex items-center gap-3">
-                      <h2 className="text-xl font-bold">Total Balance</h2>
-                      <TooltipProvider>
-                        <Tooltip>
-                          <TooltipTrigger>
-                            <i className="ri-information-line text-gray-400 font-light text-lg"></i>
-                          </TooltipTrigger>
-                          <TooltipContent className="w-52 font-normal tracking-normal">
-                            <p>
-                              Total Balance: The overall sum of all your account
-                              balances, including cash, investments, and
-                              savings.
-                            </p>
-                          </TooltipContent>
-                        </Tooltip>
-                      </TooltipProvider>
-                    </div>
-                  </div>
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <h1 className="text-4xl font-extrabold">$12,456,315</h1>
-              </CardContent>
-              <CardFooter>
-                <div className="flex items-center gap-4 w-full">
-                  <button className="bg-primary text-white px-4 py-3 rounded-lg w-full hover:opacity-80 flex items-center gap-3 justify-center">
-                    <img src={PaymentTransfer} alt="" />
-                    Transfer
-                  </button>
-                  <button className="bg-primary text-white px-4 py-3 rounded-lg w-full hover:opacity-80 flex items-center gap-3 justify-center">
-                    <img src={PaymentRecive} alt="" />
-                    Receive
-                  </button>
-                </div>
-              </CardFooter>
-            </Card>
-            <div className="col-span-2">
+            <div className="grid grid-cols-1 items-start justify-start flex-col gap-8">
               <Card className="border-0">
+                <CardHeader>
+                  <CardTitle>
+                    <div className="flex items-center justify-between mb-3">
+                      <div className="flex items-center gap-3">
+                        <h2 className="text-xl font-bold">Total Balance</h2>
+                        <TooltipProvider>
+                          <Tooltip>
+                            <TooltipTrigger>
+                              <i className="ri-information-line text-gray-400 font-light text-lg"></i>
+                            </TooltipTrigger>
+                            <TooltipContent className="w-52 font-normal tracking-normal">
+                              <p>
+                                Total Balance: The overall sum of all your
+                                account balances, including cash, investments,
+                                and savings.
+                              </p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </div>
+                    </div>
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <h1 className="text-4xl font-extrabold">$12,456,315</h1>
+                </CardContent>
+                <CardFooter>
+                  <div className="flex items-center gap-4 w-full">
+                    <button className="bg-primary text-white px-4 py-3 rounded-lg w-full hover:opacity-80 flex items-center gap-3 justify-center">
+                      <img src={PaymentTransfer} alt="" />
+                      Transfer
+                    </button>
+                    <button className="bg-primary text-white px-4 py-3 rounded-lg w-full hover:opacity-80 flex items-center gap-3 justify-center">
+                      <img src={PaymentReceive} alt="" />
+                      Receive
+                    </button>
+                  </div>
+                </CardFooter>
+              </Card>
+            </div>
+            <div className="grid grid-cols-1 items-start justify-start col-span-2 gap-8">
+              <Card className="border-0 w-full">
                 <CardHeader>
                   <CardTitle>
                     <div className="flex items-center justify-between mb-3">
@@ -151,124 +166,165 @@ const Dashboard: React.FC = () => {
                       Transfer
                     </button>
                     <button className="bg-primary text-white px-4 py-3 rounded-lg w-full hover:opacity-80 flex items-center gap-3 justify-center">
-                      <img src={PaymentRecive} alt="" />
+                      <img src={PaymentReceive} alt="" />
+                      Receive
+                    </button>
+                  </div>
+                </CardFooter>
+              </Card>
+              <Card className="border-0">
+                <CardHeader>
+                  <CardTitle>
+                    <div className="flex items-center justify-between mb-3">
+                      <div className="flex items-center gap-3">
+                        <h2 className="text-xl font-bold">Income Analysis</h2>
+                        <TooltipProvider>
+                          <Tooltip>
+                            <TooltipTrigger>
+                              <i className="ri-information-line text-gray-400 font-light text-lg"></i>
+                            </TooltipTrigger>
+                            <TooltipContent className="w-52 font-normal tracking-normal">
+                              <p>
+                                Gain insights into your earnings with detailed
+                                reports on your income streams, growth trends,
+                                and potential areas for improvement.
+                              </p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </div>
+                    </div>
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <h1 className="text-4xl font-extrabold">$12,456,315</h1>
+                </CardContent>
+                <CardFooter>
+                  <div className="flex items-center gap-4 w-full">
+                    <button className="bg-primary text-white px-4 py-3 rounded-lg w-full hover:opacity-80 flex items-center gap-3 justify-center">
+                      <img src={PaymentTransfer} alt="" />
+                      Transfer
+                    </button>
+                    <button className="bg-primary text-white px-4 py-3 rounded-lg w-full hover:opacity-80 flex items-center gap-3 justify-center">
+                      <img src={PaymentReceive} alt="" />
+                      Receive
+                    </button>
+                  </div>
+                </CardFooter>
+              </Card>
+              <Card className="border-0">
+                <CardHeader>
+                  <CardTitle>
+                    <div className="flex items-center justify-between mb-3">
+                      <div className="flex items-center gap-3">
+                        <h2 className="text-xl font-bold">Expense Analysis</h2>
+                        <TooltipProvider>
+                          <Tooltip>
+                            <TooltipTrigger>
+                              <i className="ri-information-line text-gray-400 font-light text-lg"></i>
+                            </TooltipTrigger>
+                            <TooltipContent className="w-52 font-normal tracking-normal">
+                              <p>
+                                Detailed examination of your spending patterns,
+                                helping you identify trends, manage budgets, and
+                                control expenses effectively.
+                              </p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </div>
+                    </div>
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <h1 className="text-4xl font-extrabold">$12,456,315</h1>
+                </CardContent>
+                <CardFooter>
+                  <div className="flex items-center gap-4 w-full">
+                    <button className="bg-primary text-white px-4 py-3 rounded-lg w-full hover:opacity-80 flex items-center gap-3 justify-center">
+                      <img src={PaymentTransfer} alt="" />
+                      Transfer
+                    </button>
+                    <button className="bg-primary text-white px-4 py-3 rounded-lg w-full hover:opacity-80 flex items-center gap-3 justify-center">
+                      <img src={PaymentReceive} alt="" />
                       Receive
                     </button>
                   </div>
                 </CardFooter>
               </Card>
             </div>
-            <Card className="border-0">
-              <CardHeader>
-                <CardTitle>
-                  <div className="flex items-center justify-between mb-3">
-                    <div className="flex items-center gap-3">
-                      <h2 className="text-xl font-bold">My Pocked Plans</h2>
-                      <TooltipProvider>
-                        <Tooltip>
-                          <TooltipTrigger>
-                            <i className="ri-information-line text-gray-400 font-light text-lg"></i>
-                          </TooltipTrigger>
-                          <TooltipContent className="w-52 font-normal tracking-normal">
-                            <p>
-                              My Pocket Plans: Personalized financial strategies
-                              and savings plans tailored to help you achieve
-                              your financial goals.
-                            </p>
-                          </TooltipContent>
-                        </Tooltip>
-                      </TooltipProvider>
+            <div className="grid grid-cols-1 items-start justify-start flex-col gap-8">
+              <Card className="border-0">
+                <CardHeader>
+                  <CardTitle>
+                    <div className="flex items-center justify-between mb-3">
+                      <div className="flex items-center gap-3">
+                        <h2 className="text-xl font-bold">My Pocked Plans</h2>
+                        <TooltipProvider>
+                          <Tooltip>
+                            <TooltipTrigger>
+                              <i className="ri-information-line text-gray-400 font-light text-lg"></i>
+                            </TooltipTrigger>
+                            <TooltipContent className="w-52 font-normal tracking-normal">
+                              <p>
+                                My Pocket Plans: Personalized financial
+                                strategies and savings plans tailored to help
+                                you achieve your financial goals.
+                              </p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </div>
+                      <button className="text-gray-400 text-sm flex items-start gap-2">
+                        <span>See More</span>
+                        <i className="ri-arrow-right-line"></i>
+                      </button>
                     </div>
-                    <button className="text-gray-400 text-sm flex items-start gap-2">
-                      <span>See More</span>
-                      <i className="ri-arrow-right-line"></i>
-                    </button>
-                  </div>
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <div className="grid grid-cols-2 gap-6">
-                  <div className="border border-secondary-200 p-5 rounded-xl flex flex-col items-start gap-6">
-                    <div className="w-14 h-14 rounded-lg bg-secondary-200"></div>
-                    <div>
-                      <h1 className="font-semibold text-lg">New Car</h1>
-                      <p className="text-secondary-300">$5.000,00</p>
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div className="grid grid-cols-2 gap-6">
+                    <div className="border border-secondary-200 p-5 rounded-xl flex flex-col items-start gap-6">
+                      <div className="w-14 h-14 rounded-lg bg-secondary-200"></div>
+                      <div>
+                        <h1 className="font-semibold text-lg">New Car</h1>
+                        <p className="text-secondary-300">$5.000,00</p>
+                      </div>
+                    </div>
+                    <div className="border border-secondary-200 p-5 rounded-xl flex flex-col items-start gap-6">
+                      <div className="w-14 h-14 rounded-lg bg-secondary-200"></div>
+                      <div>
+                        <h1 className="font-semibold text-lg">New Car</h1>
+                        <p className="text-secondary-300">$5.000,00</p>
+                      </div>
+                    </div>
+                    <div className="border border-secondary-200 p-5 rounded-xl flex flex-col items-start gap-6">
+                      <div className="w-14 h-14 rounded-lg bg-secondary-200"></div>
+                      <div>
+                        <h1 className="font-semibold text-lg">New Car</h1>
+                        <p className="text-secondary-300">$5.000,00</p>
+                      </div>
+                    </div>
+                    <div className="border border-secondary-200 p-5 rounded-xl flex flex-col items-start gap-6">
+                      <div className="w-14 h-14 rounded-lg bg-secondary-200"></div>
+                      <div>
+                        <h1 className="font-semibold text-lg">New Car</h1>
+                        <p className="text-secondary-300">$5.000,00</p>
+                      </div>
                     </div>
                   </div>
-                  <div className="border border-secondary-200 p-5 rounded-xl flex flex-col items-start gap-6">
-                    <div className="w-14 h-14 rounded-lg bg-secondary-200"></div>
-                    <div>
-                      <h1 className="font-semibold text-lg">New Car</h1>
-                      <p className="text-secondary-300">$5.000,00</p>
-                    </div>
-                  </div>
-                  <div className="border border-secondary-200 p-5 rounded-xl flex flex-col items-start gap-6">
-                    <div className="w-14 h-14 rounded-lg bg-secondary-200"></div>
-                    <div>
-                      <h1 className="font-semibold text-lg">New Car</h1>
-                      <p className="text-secondary-300">$5.000,00</p>
-                    </div>
-                  </div>
-                  <div className="border border-secondary-200 p-5 rounded-xl flex flex-col items-start gap-6">
-                    <div className="w-14 h-14 rounded-lg bg-secondary-200"></div>
-                    <div>
-                      <h1 className="font-semibold text-lg">New Car</h1>
-                      <p className="text-secondary-300">$5.000,00</p>
-                    </div>
-                  </div>
-                </div>
-              </CardContent>
-            </Card>
+                </CardContent>
+              </Card>
+            </div>
           </div>
         </TabsContent>
         <TabsContent value="Transaction">
           <div className="grid grid-cols-1 bg-white rounded-lg p-6">
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="w-[100px]">Invoice</TableHead>
-                  <TableHead>Status</TableHead>
-                  <TableHead>Method</TableHead>
-                  <TableHead className="text-right">Amount</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                <TableRow>
-                  <TableCell className="font-medium">INV001</TableCell>
-                  <TableCell>Paid</TableCell>
-                  <TableCell>Credit Card</TableCell>
-                  <TableCell className="text-right">$250.00</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
+            <DataTable columns={columns} data={data} />
           </div>
         </TabsContent>
         <TabsContent value="Statistics">
           <div className="grid grid-cols-3 max-md:grid-cols-1 gap-8">
-            <Card>
-              <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <p>Card Content</p>
-              </CardContent>
-              <CardFooter>
-                <p>Card Footer</p>
-              </CardFooter>
-            </Card>
-            <Card>
-              <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <p>Card Content</p>
-              </CardContent>
-              <CardFooter>
-                <p>Card Footer</p>
-              </CardFooter>
-            </Card>
             <Card>
               <CardHeader>
                 <CardTitle>Card Title</CardTitle>
--- src/components/payments/DataTable.tsx
+++ src/components/payments/DataTable.tsx
@@ -1,10 +1,13 @@
 "use client";
-
+import React from "react";
 import {
   ColumnDef,
+  SortingState,
   flexRender,
   getCoreRowModel,
+  getSortedRowModel,
   useReactTable,
+  getPaginationRowModel,
 } from "@tanstack/react-table";
 
 import {
@@ -15,6 +18,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -25,16 +29,23 @@ export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
   });
 
   return (
-    <div className="rounded-md border">
+    <>
       <Table>
-        <TableHeader>
+        <TableHeader className="bg-secondary-100">
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => {
@@ -75,6 +86,24 @@ export function DataTable<TData, TValue>({
           )}
         </TableBody>
       </Table>
-    </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
+    </>
   );
 }
